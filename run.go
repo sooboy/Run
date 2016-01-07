7510a6a84bc23d4ac8599c24d44dd5769b55a454
Regan <Regan.ni@analyticservice.net>
2016-01-07 20:03:54 +0800

    最小并发单元

---
+++ run.go
@@ -0,0 +1,55 @@
+package Run
+
+import (
+	"sync"
+)
+
+type Run struct {
+	in     chan interface{}              // 接受传入数据
+	result chan interface{}              //返回产生结果
+	wait   sync.WaitGroup                //处理多个Goroutine
+	deal   func(interface{}) interface{} //处理函数
+}
+
+func (r *Run) In(item interface{}) {
+	r.in <- item
+}
+func (r *Run) Result() chan interface{} {
+	return r.result
+}
+func (r *Run) CloseInChan() {
+	close(r.in)
+}
+func (r *Run) CloseResultChan() {
+	close(r.result)
+}
+func (r *Run) Deal() {
+	r.wait.Add(1)
+	go func() {
+		for item := range r.in {
+			r.result <- r.deal(item)
+		}
+		r.wait.Done()
+	}()
+}
+func (r *Run) DealWith(num int) {
+	for i := 0; i < num; i++ {
+		r.Deal()
+	}
+}
+func (r *Run) Wait() {
+	r.CloseInChan()
+	r.wait.Wait()
+	r.CloseResultChan()
+}
+func NewRun(DealFn func(interface{}) interface{}) *Run {
+	return &Run{
+		in:     make(chan interface{}),
+		result: make(chan interface{}),
+		deal:   DealFn,
+	}
+}
+
+func DealData(data interface{}) interface{} {
+	return data
+}

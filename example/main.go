9de1b9d8ce7fda2f1da4b1f75cf5fe3f713a6aed
Regan <Regan.ni@analyticservice.net>
2016-01-09 16:20:22 +0800

    添加功能

--- example/main.go
+++ example/main.go
@@ -3,34 +3,40 @@ package main
 import (
 	"github.com/sooboy/Run"
 	"fmt"
+	"time"
 )
+
 func DealData(data interface{}) interface{} {
+	//fmt.Println(data, "第一层处理")
 	return data
 }
 
 func main() {
+	start := time.Now()
 	//实例化一个新的Run
 	var run = Run.NewRun(DealData)
-	//处理结果
-	go func() {
-		for {
-			select {
-			case item, ok := <-run.Result():
-				if ok {
-					fmt.Println(item)
-				} else {
-					return
-				}
-			}
-		}
-	}()
+
+	//结果收集
+	var dealresult = func(data interface{}) interface{} {
+		//	fmt.Println(data, "deal 2")
+		return nil
+	}
+
+	var result = Run.NewRun(dealresult)
+	result.DealWith(5)
+	result.ChanIn(run.Result())
+
 	//开启5个Goroutine并发处理
 	run.DealWith(5)
 	//发送10个数据
-	for i := 0; i < 100; i++ {
-		run.In(true)
+
+	for i := 0; i < 10000000; i++ {
+		run.In("good")
 	}
 
+	run.InOver()
 	run.Wait()
-
+	//fmt.Println("结果接受完毕，处理第三层！")
+	result.Wait()
+	fmt.Println(time.Now().Sub(start))
 }
--- run.go
+++ run.go
@@ -6,6 +6,7 @@ import (
 
 type Run struct {
 	in     chan interface{}              // 接受传入数据
+	inover chan bool                     //接受数据完毕
 	result chan interface{}              //返回产生结果
 	wait   sync.WaitGroup                //处理多个Goroutine
 	deal   func(interface{}) interface{} //处理函数
@@ -14,39 +15,66 @@ type Run struct {
 func (r *Run) In(item interface{}) {
 	r.in <- item
 }
+func (r *Run) InOver() {
+	go func() {
+		r.inover <- true
+	}()
+}
+func (r *Run) ChanIn(in chan interface{}) {
+	go func() {
+		for item := range in {
+			r.in <- item
+		}
+		r.inover <- true
+	}()
+}
+
 func (r *Run) Result() chan interface{} {
 	return r.result
 }
 func (r *Run) CloseInChan() {
 	close(r.in)
 }
+
+func (r *Run) CloseInOverChan() {
+	close(r.inover)
+}
+
 func (r *Run) CloseResultChan() {
 	close(r.result)
 }
+
 func (r *Run) Deal() {
 	r.wait.Add(1)
 	go func() {
 		for item := range r.in {
-			r.result <- r.deal(item)
+			result := r.deal(item)
+			if result != nil {
+				r.result <- result
+			}
 		}
 		r.wait.Done()
 	}()
 }
+
 func (r *Run) DealWith(num int) {
 	for i := 0; i < num; i++ {
 		r.Deal()
 	}
 }
+
 func (r *Run) Wait() {
 	r.CloseInChan()
 	r.wait.Wait()
 	r.CloseResultChan()
+	<-r.inover
 }
+
 func NewRun(DealFn func(interface{}) interface{}) *Run {
 	return &Run{
 		in:     make(chan interface{}),
+		inover: make(chan bool, 1),
 		result: make(chan interface{}),
 		deal:   DealFn,
 	}
 }
-
